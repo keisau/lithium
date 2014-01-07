fe3f5eb7dd71a2bab7667aac376bfaad3a49a290
pierresaux <pierresaux@ymail.com>
2014-01-07 16:41:20 +0800

    Added erase test cases

--- radix_tree_test.cc
+++ radix_tree_test.cc
@@ -1,7 +1,9 @@
 #include "lithium.h"
 #include "radix_tree.h"
 #include <string>
+#include <stdio.h>
 using namespace std;
+using namespace li;
 
 #define BUF_SIZE 2048
 int main ()
@@ -13,6 +15,10 @@ int main ()
 		sprintf (buf, "%llu", 1ULL << i);
 		tree.insert (1ULL << i, string (buf));
 	}
+
+	tree.erase (tree.find (1ULL << 36));
+	tree.erase (tree.find (1ULL << 26));
+	tree.erase (tree.find (1ULL << 6));
 
 	for (radix_tree<string>::iterator it = tree.begin();
 			it != tree.end();
