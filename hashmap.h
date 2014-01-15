c36c77092a1fb4a97ab50551515ba1df535e72a3
pierresaux <pierresaux@ymail.com>
2014-01-15 17:56:08 +0800

    Added item node for iterator implementation

--- hashmap.h
+++ hashmap.h
@@ -1,28 +1,69 @@
 #ifndef __LITHIUM_HASHMAP_H__
 #define __LITHIUM_HASHMAP_H__
+
+#include "lithium.h"
 #include "hash.h"
 #include "hlist.h"
-#include "radix_tree.h"
+#include "list.h"
 
-template <typename _KeyType,
-		 typename _ValType,
-		 class _Less = less<_KeyType>>
-class hashmap
+namespace li
 {
-public:
-	class iterator;
-	friend class iterator;
+	template <typename _KeyType, typename _ValType, class _Traits = li::_naive_type_trait <_KeyType> >
+		class hashmap
+		{
+		public:
+			class iterator;
+			friend class iterator;
 
-protected:
-	typedef std::pair <_KeyType, _ValType> _ItemType;
+			// typedefs
+		protected:
+			struct item_node
+			{
+				_KeyType			key;
+				_ValType			value;
+				list_head			head;
+			};
 
-public:
-	hashmap () {}
-	~hashmap () {}
+			typedef typename _Traits::type_t				__hash_key_t;
 
-protected:
+			// public methods
+		public:
+			hashmap () : _size (4096) {
+				_table = new hlist <item_node> [4097];
+				_table [4096] = NULL;
+			}
 
-protected:
-	radix_tree <hlist> _RadixMap;
-};
+			hashmap (size_t size) : _size (size) {
+				_table = new hlist <item_node> [_size + 1];
+				_table [size] = NULL;
+			}
+
+			~hashmap () {}
+
+			void insert (_KeyType _key, _ValType val)
+			{
+				__hash_key_t key = _Traits ()(_key);
+				index_t _x = get_hash (_key);
+				_table [_x].insert (make_pair (key, val));
+			}
+
+			bool find (_KeyType _key)
+			{
+				__hash_key_t key = _Traits ()(_key);
+				index_t _x = get_hash (_key);
+			}
+
+			// protected methods
+		protected:
+			index_t get_hash (__hash_key_t hash_key) {
+				index_t retval = _hash (hash_key);
+				return retval % _size;
+			}
+
+			// attributes
+		protected:
+			size_t					_size;
+			li::hlist <item_node>		*_table;
+		};
+}
 #endif // __LITHIUM_HASHMAP_H__
