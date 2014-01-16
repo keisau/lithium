5de6b96a1d09fb6d53cc4bf258d6f6dce160c515
pierresaux <pierresaux@ymail.com>
2014-01-16 14:43:37 +0800

    Added best-effort support of container_of for windows

--- lithium.h
+++ lithium.h
@@ -1,3 +1,19 @@
+/**
+ * This file is part of lithium.
+ *
+ * lithium is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * lithium is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lithium.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef __LITHIUM_H__
 #define __LITHIUM_H__
 
@@ -49,8 +65,13 @@ namespace li
  * The container_of macro - modified to fit into non-gnu compilers without
  * the typeof keyword.
  */
+#ifndef _WIN32
 #define container_of(ptr, type, member) ({							\
 		const char *__mptr = (char*) (ptr);			\
 		(type *) (__mptr - offsetof (type, member)); })
+#else
+#define container_of(ptr, type, member)			\
+	((type *) ((char*) (ptr) - offsetof (type, member)))
+#endif
 
 #endif // __LITHIUM_H__
