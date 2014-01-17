4ec7f6f6f0d5abdd7fdd0ac7e4cf5a7fa36d6607
pierresaux <pierresaux@ymail.com>
2014-01-17 17:13:09 +0800

    More test cases

--- radix_tree_test.cc
+++ radix_tree_test.cc
@@ -15,10 +15,26 @@ int main ()
 		sprintf (buf, "%llu", 1ULL << i);
 		tree.insert (1ULL << i, string (buf));
 	}
-
+	
+	//tree.erase (1ULL << 36);
+	//tree.erase (1ULL << 26);
+	//tree.erase (1ULL << 6);
+	
 	tree.erase (tree.find (1ULL << 36));
 	tree.erase (tree.find (1ULL << 26));
 	tree.erase (tree.find (1ULL << 6));
+
+	radix_tree<string>::iterator it;
+
+	for (int i = 0; i < 64; ++i)
+	{
+		sprintf (buf, "%llu", 1ULL << i);
+		string str = string (buf);
+		if (tree.find (1ULL << i) != tree.end())
+		{
+			puts (tree[1ULL << i].c_str());
+		}
+	}
 
 	for (radix_tree<string>::iterator it = tree.begin();
 			it != tree.end();
