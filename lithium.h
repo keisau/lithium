77113372430b5d8acdf71e27e1c09cbc94f135cc
pierresaux <pierresaux@ymail.com>
2014-01-17 17:02:52 +0800

    Update lithium.h

--- lithium.h
+++ lithium.h
@@ -1,119 +1,47 @@
-/**
- * This file is part of lithium.
- *
- * lithium is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * lithium is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with lithium.  If not, see <http://www.gnu.org/licenses/>.
- */
-#ifndef __LITHIUM_H__
-#define __LITHIUM_H__
+#include "lithium.h"
+#include "radix_tree.h"
+#include <string>
+#include <stdio.h>
+using namespace std;
+using namespace li;
 
-#include "types.h"
-
-// lithium library templates
-namespace li
+#define BUF_SIZE 2048
+int main ()
 {
-	template <typename _Type>
-		struct traits 
-		{
-			typedef _Type			type_t;
-			typedef _Type			*ptr_t;
-
-			static size_t get_size (const ptr_t _in) {
-				return sizeof (type_t);
-			}
-
-			static ptr_t get_key (const _Type &_in) {
-				return  (ptr_t) &_in;
-			}
-		};
-
-#pragma pack (push, 1)
-	template <typename _Type1, typename _Type2>
-		struct pair
-		{
-			_Type1		first;
-			_Type2		second;
-			pair& operator= (const pair& pr) { 
-				first = pr.first; second = pr.second;
-				return *this;
-			}
-
-			bool operator== (const pair& rvalue) {
-				return first == rvalue.first && second == rvalue.second;
-			}
-
-			bool operator!= (const pair& rvalue) {
-				return !(*this == rvalue);
-			}
-
-			bool operator<  (const pair& rvalue) { 
-				return first < rvalue.first 
-					|| (rvalue.first >= first && second < rvalue.second); 
-			}
-
-			bool operator<= (const pair& rvalue) {
-				return !(rvalue < *this);
-			}
-
-			bool operator>  (const pair& rvalue) {
-				return rvalue < *this; 
-			}
-
-			bool operator>= (const pair& rvalue) {
-				return !(*this < rvalue); 
-			}
-		};
-
-#pragma pack (pop)
-
-	template <typename _Type1, typename _Type2>
-		li::pair <_Type1, _Type2> make_pair (_Type1 x, _Type2 y)
-		{
-			li::pair<_Type1, _Type2> retval;
-			retval.first = x;
-			retval.second = y;
-			return retval;
-		}
-}
-
-// utility functions
-namespace li
-{
-	static inline index_t div_round_up (index_t ll, index_t d)
+	radix_tree<string> tree;
+	char buf [BUF_SIZE];
+	for (int i = 0; i < 64; ++i)
 	{
-		index_t tmp = ll + d - 1;
-		return tmp / d;
+		sprintf (buf, "%llu", 1ULL << i);
+		tree.insert (1ULL << i, string (buf));
 	}
+	
+	//tree.erase (1ULL << 36);
+	//tree.erase (1ULL << 26);
+	//tree.erase (1ULL << 6);
+	
+	tree.erase (tree.find (1ULL << 36));
+	tree.erase (tree.find (1ULL << 26));
+	tree.erase (tree.find (1ULL << 6));
+
+	radix_tree<string>::iterator it;
+
+	for (int i = 0; i < 64; ++i)
+	{
+		sprintf (buf, "%llu", 1ULL << i);
+		string str = string (buf);
+		if (tree.find (1ULL << i) != tree.end())
+		{
+			puts (tree[1ULL << i].c_str());
+		}
+	}
+
+	for (radix_tree<string>::iterator it = tree.begin();
+			it != tree.end();
+			++it)
+	{
+		printf ("%s\n", (*it).c_str());
+	}
+
+	return 0;
 }
-
-// utility macros
-/**
- * The offsetof macro - hoped to eliminate Winvalid-offsetof warning
- */
-#undef offsetof
-#define offsetof(type, member) ((size_t) &(((type *)0)->member))
-
-/**
- * The container_of macro - modified to support compilers without
- * the typeof keyword.
- */
-#ifndef _WIN32
-#define container_of(ptr, type, member) ({						\
-		const char *__mptr = (char*) (ptr);						\
-		(type *) (__mptr - offsetof (type, member)); })
-#else
-#define container_of(ptr, type, member)							\
-	((type *) ((char*) (ptr) - offsetof (type, member)))
-#endif
-
-#endif // __LITHIUM_H__
