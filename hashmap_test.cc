e47d141c9c1f7a19a9cd3aaaac1d1f0a7d8d07e3
pierresaux <pierresaux@ymail.com>
2014-01-23 15:35:20 +0800

    More sophisticated test cases

--- hashmap_test.cc
+++ hashmap_test.cc
@@ -16,6 +16,11 @@ int main ()
 	hashmap <string, string>::iterator it;
 
 	_map2.insert (1, 1);
+	_map = hashmap <string, string> ();
+	for (it = _map.begin (); it != _map.end (); ++it) {
+		printf ("1 << %s = %s\n", it->first.c_str (), it->second.c_str());
+	}
+
 	for (u64 i = 0, a = 1; i < 64; ++i, a <<= 1)
 	{
 		sprintf (buf1, "%llu", i);
@@ -26,15 +31,29 @@ int main ()
 	for (u64 i = 0, a = 1; i < 64; ++i, a <<= 1)
 	{
 		sprintf (buf1, "%llu", i);
-		puts (_map[string (buf1)].c_str ());
+		string str = string (buf1);
+		puts (_map[str].c_str ());
+		puts (buf1);
 	}
-
+	
+	_map.clear();
+	if (_map.find (string ("1")) == _map.end())
+		puts ("gd");
+	else
+		puts ("bad");
 	_map.erase (_map.find (string ("1")));
 	_map.erase (_map.find (string ("6")));
+	_map.erase (string ("0"));
+	_map.erase (string ("1"));
+	_map.erase (string ("2"));
+	_map.erase (string ("3"));
+	_map.erase (string ("4"));
+	_map.erase (string ("5"));
 	_map.erase (string ("16"));
 
-	for (it = _map.begin (); it != _map.end (); ++it)
+	for (it = _map.begin (); it != _map.end (); ++it) {
 		printf ("1 << %s = %s\n", it->first.c_str (), it->second.c_str());
+	}
 
 	return 0;
 }
